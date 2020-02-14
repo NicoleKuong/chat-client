0ca94c6d1f9d1d71bbd7c85288f389f7ceae6047
NicoleKuong <nicolekuong@gmail.com>
2020-02-14 14:02:30 +0100

    view messages

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React from "react";
 
 import "./App.css";
 
-//event source to connect to stream
-
 class App extends React.Component {
+  state = {
+    messages: []
+  };
   stream = new EventSource("http://localhost:4000/stream");
 
   componentDidMount() {
-    this.stream.onmessage = function(event) {
+    //event source to connect to stream
+    this.stream.onmessage = event => {
       console.log("event test:", event);
       const { data } = event;
       console.log("data test", data);
       const parsed = JSON.parse(data);
       console.log("parsed test", parsed);
+
+      this.setState({ messages: parsed });
     };
   }
   render() {
-    return <div>Hello</div>;
+    const paragraphs = this.state.messages.map(
+      message => <p key={message.id}>{message.text}</p> //show which of data
+    );
+    return <div>{paragraphs}</div>;
   }
 }
 
