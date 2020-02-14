7b278574cd650313e1ab12d062aee9ef71670f5b
NicoleKuong <nicolekuong@gmail.com>
2020-02-14 15:20:00 +0100

    create form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-
 import "./App.css";
 
 class App extends React.Component {
   state = {
-    messages: []
+    messages: [],
+    value: ""
   };
   stream = new EventSource("http://localhost:4000/stream");
 
@@ -14,17 +14,56 @@ class App extends React.Component {
       console.log("event test:", event);
       const { data } = event;
       console.log("data test", data);
-      const parsed = JSON.parse(data);
-      console.log("parsed test", parsed);
+      const action = JSON.parse(data);
+      console.log("action test", action);
 
-      this.setState({ messages: parsed });
+      const { type, payload } = action;
+
+      if (type === "ALL_MESSAGES") {
+        //messages returns an array, replace the whole array
+        this.setState({ messages: payload });
+      }
+      if (type === "ONE_MESSAGE") {
+        // message return a single object, add it to the array
+        const messages = [...this.state.messages, payload];
+        this.setState({ messages });
+      }
     };
   }
+
+  onSubmit = event => {
+    event.preventDefault();
+    console.log("value test", this.state.value);
+  };
+
+  onChange = event => {
+    const { value } = event.target;
+    this.setState({ value });
+  };
+
+  clear = () => {
+    this.setState({ value: "" }); //clear the form
+  };
+
   render() {
+    console.log("render state test:", this.state);
     const paragraphs = this.state.messages.map(
       message => <p key={message.id}>{message.text}</p> //show which of data
     );
-    return <div>{paragraphs}</div>;
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            onChange={this.onChange}
+            type="text"
+            value={this.state.value}
+          />
+          <button>submit</button>
+          <button onClick={this.clear}>Clear</button>
+        </form>
+        {paragraphs}
+      </div>
+    );
   }
 }
 
