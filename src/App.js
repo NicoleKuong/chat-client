9da2e3167bb849cd8362f7ef7c1535aac0f10739
NicoleKuong <nicolekuong@gmail.com>
2020-02-14 13:50:12 +0100

    connect stream to react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+//event source to connect to stream
+
+class App extends React.Component {
+  stream = new EventSource("http://localhost:4000/stream");
+
+  componentDidMount() {
+    this.stream.onmessage = function(event) {
+      console.log("event test:", event);
+      const { data } = event;
+      console.log("data test", data);
+      const parsed = JSON.parse(data);
+      console.log("parsed test", parsed);
+    };
+  }
+  render() {
+    return <div>Hello</div>;
+  }
 }
 
 export default App;
